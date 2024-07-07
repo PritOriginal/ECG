0d3ed7ac68c8544fdb5bd6a729727cd7e38601af
PritOriginal <72299692+PritOriginal@users.noreply.github.com>
2024-07-07 21:52:56 +0300

    v1

--- learn.py
+++ learn.py
@@ -1,8 +1,17 @@
-# Подкласс QMainWindow для настройки главного окна приложения
 import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+from decimal import Decimal
+
+from scipy.interpolate import interp1d
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 from PySide6.QtWidgets import QMainWindow, QPushButton, QApplication, QFileDialog
 from ui_main import Ui_MainWindow
@@ -18,7 +27,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.filePath = Path()
         self.data = pd.DataFrame()
         self.selectFileBtn.clicked.connect(self.selectFile)
-        self.graphsBtn.clicked.connect(self.showGraphs)
+        self.ecgCurveBtn.clicked.connect(self.showEcgGraph)
+        self.heartRateBtn.clicked.connect(self.showHeartRateGraph)
+        self.splitBtn.clicked.connect(self.splitCsv)
+        self.learnBtn.clicked.connect(self.learn)
+
+        self.learn()
 
     def selectFile(self):
         dialog = QFileDialog(self)
@@ -31,28 +45,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.readCsv()
             else:
                 self.readTxt()
-            self.graphsBtn.setEnabled(True)
+
+            time_start = self.data.iloc[0, 0]
+            self.data['timestamp:'] -= time_start
+
+            self.ecgCurveBtn.setEnabled(True)
+            self.heartRateBtn.setEnabled(True)
+            self.splitBtn.setEnabled(True)
 
     def readCsv(self):
         self.data = pd.read_csv(self.filePath)
 
     def readTxt(self):
         with open(self.filePath, 'r') as f:
+            data_list = []
             rows = f.read().split('\n')
-            self.data = pd.DataFrame(columns=rows[0].split())
+            columns = rows[0].split()
             rows = rows[1:]
             for row in rows:
                 if row:
                     data_row = row.split()
                     data_row[0] = float(data_row[0][:-1].replace(',', '.'))
-                    self.data.loc[len(self.data)] = data_row
-            self.data.to_csv(self.filePath.stem + '.csv', index=False)
-    def showGraphs(self):
-        print(self.data)
-        time_start = self.data.iloc[0, 0]
-        print(time_start)
-        self.data['timestamp:'] -= time_start
+                    data_list.append(data_row)
+            self.data = pd.DataFrame(data_list, columns=columns, dtype='float64')
 
+    def showEcgGraph(self):
         all_time = np.array([])
         time_0 = self.data['timestamp:'][0]
 
@@ -60,26 +77,149 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for index in range(len(self.data['timestamp:'])):
             time = self.data['timestamp:'][index]
             if time != time_0 or index == len(self.data['timestamp:']) - 1:
-                all_time = np.hstack([all_time, np.linspace(time_0, time, i)])
+                if index != len(self.data['timestamp:']) - 1:
+                    all_time = np.hstack([all_time, np.linspace(time_0, time, i)])
+                else:
+                    all_time = np.hstack([all_time, np.linspace(time_0, time_0 + 0.03, i)])
                 time_0 = time
                 i = 1
             else:
                 i += 1
 
-        fig, ax = plt.subplots(2, 1)
-        ax[0].plot(all_time, self.data['HeartRate4sAverage'].to_numpy(dtype=int), label='Heart Rate 4s Average')
-        ax[0].plot(all_time, self.data['HeartRate30sAverage'].to_numpy(dtype=int), label='Heart Rate 30s Average')
-        ax[0].set_xlabel('Time')
-        ax[0].set_ylabel('HeartRate')
-        ax[0].grid(True)
-        ax[0].legend()
+        fig, ax = plt.subplots()
 
-        ax[1].plot(all_time, self.data['ADC'].to_numpy(dtype=int))
-        ax[1].set_xlabel('Time')
-        ax[1].set_ylabel('ADC')
-        ax[1].grid(True)
-
+        ax.plot(all_time, self.data['ADC'].to_numpy(dtype=int))
+        ax.set_xlabel('Time')
+        ax.set_ylabel('ADC')
+        ax.grid(True)
+        fig.canvas.manager.window.showMaximized()
+        plt.subplots_adjust(bottom=0.059, top=0.985, left=0.037, right=0.992, hspace=0.2, wspace=0.2)
         plt.show()
+
+    def showHeartRateGraph(self):
+        orig_time = [self.data['timestamp:'][0]]
+
+        all_time = np.array([])
+        time_0 = self.data['timestamp:'][0]
+
+        all_heart_rate_4s = [self.data['HeartRate4sAverage'][0]]
+        all_heart_rate_30s = [self.data['HeartRate30sAverage'][0]]
+
+        i = 1
+        for index in range(len(self.data['timestamp:'])):
+            time = self.data['timestamp:'][index]
+            heart_rate_4s = self.data['HeartRate4sAverage'][index]
+            heart_rate_30s = self.data['HeartRate30sAverage'][index]
+            if time != time_0 or index == len(self.data['timestamp:']) - 1:
+                if index != len(self.data['timestamp:']) - 1:
+                    all_time = np.hstack([all_time, np.linspace(time_0, time, i)])
+                    orig_time.append(time)
+                else:
+                    all_time = np.hstack([all_time, np.linspace(time_0, time_0 + 0.03, i)])
+                    orig_time.append(time + 0.03)
+                time_0 = time
+
+                all_heart_rate_4s.append(heart_rate_4s)
+                all_heart_rate_30s.append(heart_rate_30s)
+
+                i = 1
+            else:
+                i += 1
+
+
+        split_data, split_time = self.splitData()
+        split_data = split_data.iloc[:, 2:]
+        data_pred = self.forest.predict(split_data)
+        # data_pred *= 100
+        # data_pred += 60
+
+        fig, ax = plt.subplots()
+        x_start = 0
+        x_end = 0
+        startStress = False;
+        for i in range(len(data_pred)):
+            stress = data_pred[i]
+            if stress == 1 and not startStress:
+                x_start = split_time[i]
+                startStress = True
+            elif stress == 0 and startStress:
+                x_end = split_time[i] - x_start
+                startStress = False
+                ax.add_patch(Rectangle((x_start,40),x_end,110, facecolor='r', alpha=0.2))
+
+        # ax[0].plot(all_time, all_heart_rate_4s, label='Heart Rate 4s Average')
+        ax.plot(orig_time, all_heart_rate_4s, label='Heart Rate 4s Average')
+        ax.plot(orig_time, all_heart_rate_30s, label='Heart Rate 30s Average')
+        # ax[0].plot(split_time, data_pred, label="Stress")
+        # ax[0].fill_between(split_time, y1=data_pred, y2=40)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('HeartRate')
+        ax.grid(True)
+        ax.legend()
+        fig.canvas.manager.window.showMaximized()
+        plt.subplots_adjust(bottom=0.059, top=0.985, left=0.037, right=0.992, hspace=0.2, wspace=0.2)
+        plt.show()
+
+    def splitCsv(self):
+        data, _ = self.splitData()
+        print(data)
+        data.to_csv(f'data/payload/{self.filePath.stem}.csv', index=False)
+
+    def splitData(self):
+        data = self.data.drop_duplicates(subset=['timestamp:'], ignore_index=True)
+        data.reindex()
+        data.drop('ADC', axis=1, inplace=True)
+
+        split_step = 160
+
+        split_time = []
+
+        split_data_list = []
+        columns = ['stress', 'time']
+        for j in range(split_step):
+            columns.append(f'HeartRate4sAverage{j}')
+            columns.append(f'HeartRate30sAverage{j}')
+
+        i = 0
+        while (i+1) * split_step < len(data):
+            split_time.append(data['timestamp:'][split_step*i])
+            row_data = [0,
+                        f'{round(data['timestamp:'][split_step*i], 3)}-{round(data['timestamp:'][split_step*(i+1)], 3)}']
+            # print(split_step*(i+1))
+            for ind in range(int(split_step*i), int(split_step*(i+1))):
+                row_data.append(int(data['HeartRate4sAverage'][ind]))
+                row_data.append(int(data['HeartRate30sAverage'][ind]))
+
+            split_data_list.append(row_data)
+            i += 0.5
+
+        split_data = pd.DataFrame(split_data_list, columns=columns)
+        return split_data, split_time
+
+    def learn(self):
+        frames = []
+        pathlist = Path('data/payload').glob('**/*.csv')
+        for path in pathlist:
+            frames.append(pd.read_csv(str(path)))
+        data = pd.concat(frames, axis=0)
+        data.reindex()
+
+        X = data.iloc[:, 2:]
+        y = data['stress']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=5, stratify=y)
+
+        self.forest = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+        self.forest.fit(X_train, y_train)
+
+        y_pred_forest = self.forest.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred_forest)
+        self.accuracyModelLb.setText(str(accuracy))
+        print("Accuracy:", accuracy_score(y_test, y_pred_forest))
+        print(classification_report(y_test, y_pred_forest))
+
+        self.forest = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+        self.forest.fit(X, y)
 
 
 app = QApplication(sys.argv)
--- ui_main.py
+++ ui_main.py
@@ -17,13 +17,13 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLayout,
     QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QStatusBar, QVBoxLayout, QWidget)
+    QSpacerItem, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(516, 169)
+        MainWindow.resize(516, 235)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
@@ -56,11 +56,56 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.selectFileBtn)
 
-        self.graphsBtn = QPushButton(self.centralwidget)
-        self.graphsBtn.setObjectName(u"graphsBtn")
-        self.graphsBtn.setEnabled(False)
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.heartRateBtn = QPushButton(self.centralwidget)
+        self.heartRateBtn.setObjectName(u"heartRateBtn")
+        self.heartRateBtn.setEnabled(False)
 
-        self.verticalLayout.addWidget(self.graphsBtn)
+        self.horizontalLayout_2.addWidget(self.heartRateBtn)
+
+        self.ecgCurveBtn = QPushButton(self.centralwidget)
+        self.ecgCurveBtn.setObjectName(u"ecgCurveBtn")
+        self.ecgCurveBtn.setEnabled(False)
+
+        self.horizontalLayout_2.addWidget(self.ecgCurveBtn)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.learnBtn = QPushButton(self.centralwidget)
+        self.learnBtn.setObjectName(u"learnBtn")
+
+        self.verticalLayout.addWidget(self.learnBtn)
+
+        self.splitBtn = QPushButton(self.centralwidget)
+        self.splitBtn.setObjectName(u"splitBtn")
+        self.splitBtn.setEnabled(False)
+
+        self.verticalLayout.addWidget(self.splitBtn)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_3.addWidget(self.label_2)
+
+        self.accuracyModelLb = QLabel(self.centralwidget)
+        self.accuracyModelLb.setObjectName(u"accuracyModelLb")
+
+        self.horizontalLayout_3.addWidget(self.accuracyModelLb)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -81,6 +126,11 @@ class Ui_MainWindow(object):
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u0439\u043b:", None))
         self.fileName.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0435 \u0432\u044b\u0431\u0440\u0430\u043d", None))
         self.selectFileBtn.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u0442\u044c", None))
-        self.graphsBtn.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0441\u0442\u0440\u043e\u0438\u0442\u044c \u0433\u0440\u0430\u0444\u0438\u043a", None))
+        self.heartRateBtn.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0441\u0442\u0440\u043e\u0438\u0442\u044c \u0433\u0440\u0430\u0444\u0438\u043a \u0427\u0421\u0421", None))
+        self.ecgCurveBtn.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0441\u0442\u0440\u043e\u0438\u0442\u044c \u0433\u0440\u0430\u0444\u0438\u043a \u043a\u0440\u0438\u0432\u043e\u0439", None))
+        self.learnBtn.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0431\u0443\u0447\u0438\u0442\u044c", None))
+        self.splitBtn.setText(QCoreApplication.translate("MainWindow", u"\u0420\u0430\u0437\u0431\u0438\u0442\u044c", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u0422\u043e\u0447\u043d\u043e\u0441\u0442\u044c \u043c\u043e\u0434\u0435\u043b\u0438:", None))
+        self.accuracyModelLb.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
     # retranslateUi
 
